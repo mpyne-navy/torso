ad29dbcaaca6cc03c5c24b46144421697fc44069
Michael Pyne <michael.pyne@gmail.com>
2023-08-20 17:27:40 -0400

    Use the generated personnel data in our non-existent model.

--- model.py
+++ model.py
@@ -11,6 +11,12 @@ def read_billets(filename='billets.csv'):
 
     raise OSError(f"Could not read {filename}")
 
+def read_personnel(filename='personnel.csv'):
+    ''' Reads in the given list of personnel for the HR model simulation '''
+    with open(filename, newline='') as csvfile:
+        datareader = csv.DictReader(csvfile)
+        return [row for row in datareader]
+
 if __name__ == '__main__':
     billets = read_billets()
 
@@ -18,5 +24,13 @@ if __name__ == '__main__':
         print ("Billet file empty")
         sys.exit(1)
 
+    pers    = read_personnel()
+
+    if len(billets) <= 0:
+        print ("Personnel file empty")
+        sys.exit(1)
+
     print (f"Read in {len(billets)} billets")
+    print (f"Read in {len(pers)} personnel")
+
     sys.exit(0)
